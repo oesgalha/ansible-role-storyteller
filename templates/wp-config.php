32fabf1f63ab6e410594ee5bb0a5ffa647f975bb
MDDetails <runpartner@gmail.com>
2017-01-08 22:31:25 -0500

    Fixed bad db_user_name variable in template

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', '{{ storyteller_wp_db_name }}');
 
 /** MySQL database username */
-define('DB_USER', '{{ storyteller_wp_db_name }}');
+define('DB_USER', '{{ storyteller_wp_db_user }}');
 
 /** MySQL database password */
 define('DB_PASSWORD', '{{ storyteller_wp_db_password }}');
